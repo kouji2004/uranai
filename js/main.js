89f70137f05bf50bb4ea484ccff91d2c75fb6aad
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-07 12:15:59 +0900

    HTML　修正

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const btn = document.getElementById("btn");
+const result = document.getElementById("result");
+
+btn = addEventListener("click", ()=> {
+  const num = Math.random(); //0~0.999が自動で選ばれる
+  
+  if(num<0.2){
+    result.textContent = "ラッキー";
+  }else if(num<0.5){
+    result.textContent = "まぁまぁ";
+  }else if(num<0.8){
+    result.textContent = "気を付けて";
+}else{
+  result.textContent = "やばい";
+}
+});
