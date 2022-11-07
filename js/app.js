e0d38306598b59f3909a45022fda72ebf7883c68
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-07 13:21:10 +0900

    ジャバスクリプト　for文

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
 
 
 //⓵ジャバスクリプトではletが変数
-
 // let hello = "hellooo0";
-
 // alert(hello);
 
 
-//⓶while文の記述
+// //⓶while文の記述
+// let max = 100;
+// let num = 1;
+// let count = 0;
 
-let max = 100;
-let num = 1;
-let count = 0;
+// while(num<max){
+//   num = num*2;
+//   count = count+1;
+// }
+// alert("２をかけ続けて"+max+"を超えるのに必要な回数は"+count+"回です");
 
-while(num<max){
-  num = num*2;
-  count = count+1;
+// ⓷for文
+
+let i;
+let num =0;
+
+for(i=1; i<10; i++){
+  num = num+i;
 }
-alert("２をかけ続けて"+max+"を超えるのに必要な回数は"+count+"回です");
-
+alert("１から１０まで足し算した結果は"+num+"です");
