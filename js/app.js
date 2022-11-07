9915624118cc03a22056f4c5ce7e6105727e6411
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-07 13:16:53 +0900

    while文

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 
 
-//ジャバスクリプトではletが変数
+//⓵ジャバスクリプトではletが変数
 
-let hello = "hellooo0";
+// let hello = "hellooo0";
 
-alert(hello);
+// alert(hello);
+
+
+//⓶while文の記述
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num<max){
+  num = num*2;
+  count = count+1;
+}
+alert("２をかけ続けて"+max+"を超えるのに必要な回数は"+count+"回です");
+
