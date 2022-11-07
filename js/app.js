b6efd571198881e8b163a6bb14fc4f24fea0cf10
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-07 16:10:23 +0900

    入力ダイアログで値を入力する

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 
 
-//⓵ジャバスクリプトではletが変数
-// let hello = "hellooo0";
-// alert(hello);
+// ⓵ジャバスクリプトではletが変数
+//  let hello = "hellooo0";
+//  alert(hello);
 
 
 // //⓶while文の記述
@@ -16,12 +16,31 @@
 // }
 // alert("２をかけ続けて"+max+"を超えるのに必要な回数は"+count+"回です");
 
-// ⓷for文
 
-let i;
-let num =0;
+// // ⓷for文
+// let i;
+// let num =0;
+// for(i=1; i<10; i++){
+//   num = num+i;
+// }
+// alert("１から１０まで足し算した結果は"+num+"です");
 
-for(i=1; i<10; i++){
-  num = num+i;
-}
-alert("１から１０まで足し算した結果は"+num+"です");
+// ⓸関数
+//   ↓↓↓↓
+// //メイン部分
+// let alertString;
+//  alertString = addString("ミウラ");
+// //  作成した関数を呼び出す
+// alert(alertString);
+// // 作成した関数
+// function addString(name){
+//   let name2 =name+"コウジ";
+//   return name2;
+// }
+// // 実行結果　→　ミウラコウジ
+
+
+// //入力ダイアログで値を入力する
+// let promptStr = prompt('何か好きな文字を入力してください。');
+// alert(promptStr);
+
