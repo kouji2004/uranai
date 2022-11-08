800dfd812dc6e180249137d02e1077b730f4bdb9
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-08 16:33:10 +0900

    thisを使ってslideUp

---
+++ js/main5.js
@@ -0,0 +1,31 @@
+// $(function(){
+//   $(".bg1").on("click",function(){
+//     $(".bg1").slideUp();
+//   });
+//   $(".bg2").on("click",function(){
+//     $(".bg2").slideUp();
+//   })
+//   $(".bg3").on("click",function(){
+//     $(".bg3").slideUp();
+//   })
+//   $(".bg4").on("click",function(){
+//     $(".bg4").slideUp();
+//   })
+// })
+
+//thisを使ってslideUp
+$(function(){
+  $(".bg1").on("click",function(){
+    $(this).slideUp();
+  })
+  $(".bg2").on("click",function(){
+    $(this).slideUp();
+  })
+  $(".bg3").on("click",function(){
+    $(this).slideUp();
+  })
+  $(".bg4").on("click",function(){
+    $(this).slideUp();
+  })
+})
+
