1063f48aa1e1436333eb6f3e5a54fbacfceb45ab
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-08 14:45:35 +0900

    「マウスアウト」イベントを追加する

--- js/main4.js
+++ js/main4.js
@@ -1,5 +1,18 @@
+// // jQueryで「マウスオーバー」イベントを設定する
+// $(function(){
+//   $(".box1").mouseover(function(){
+//     $(".box1").css({"background-color":"#0000ff"});
+//   });
+// });
+
+
+
+// 「マウスアウト」イベントを追加する
 $(function(){
   $(".box1").mouseover(function(){
     $(".box1").css({"background-color":"#0000ff"});
+  });
+  $(".box1").mouseout(function(){
+    $(".box1").css({"background-color":"#ff0000"});
   });
 });
